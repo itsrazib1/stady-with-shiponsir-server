d8236fdb954403c0dddaa9ee0353b6b0f6ec155e
itsrazib1 <itsrazib1@gmail.com>
2023-12-30 13:05:01 +0600

    add Patch

--- index.js
+++ index.js
@@ -75,6 +75,26 @@ async function run() {
       const result = await coffeeCollection.updateOne(filter, coffee, options);
       res.send(result);
     });
+    app.patch('/studentsetails/:id', async (req, res) => {
+      const taskId = req.params.id;
+      const updatedTaskData = req.body;
+
+      try {
+        const result = await coffeeCollection.updateOne(
+          { _id: new ObjectId(taskId) },
+          { $set: updatedTaskData }
+        );
+
+        if (result.matchedCount === 0) {
+          res.status(404).json({ error: 'Task not found' });
+        } else {
+          res.json({ message: 'Task updated successfully' });
+        }
+      } catch (err) {
+        console.error('Error updating task:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
 
     app.delete("/studentsetails/:id", async (req, res) => {
       const id = req.params.id;
